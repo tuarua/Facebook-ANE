a865fafa15e0d568bdb908d5844a79ae36059889
Eoin Landy <info@tuarua.co.uk>
2019-08-06 14:18:55 +0100

    1.0.0

--- example/get_android_dependencies.ps1
+++ example/get_android_dependencies.ps1
@@ -1,9 +1,20 @@
 $AneVersion = "1.0.0"
 $FreKotlinVersion = "1.8.0"
 $GsonVersion = "2.8.4"
+$SupportV4Version = "27.1.0"
+$LifeCycleVersion = "1.1.1"
+$BoltsVersion = "1.4.0"
+$FacebookVersion = "5.1.0"
 
 $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri https://github.com/tuarua/Facebook-ANE/releases/download/$AneVersion/BatteryANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\BatteryANE.ane"
-Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.google.code.gson.gson-$GsonVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/misc/com.google.code.gson.gson-$GsonVersion-air33.ane?raw=true
+Invoke-WebRequest -Uri https://github.com/tuarua/Facebook-ANE/releases/download/$AneVersion/FacebookANE.ane?raw=true -OutFile "$currentDir\..\native_extension\ane\FacebookANE.ane"
+Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.google.code.gson.gson-$GsonVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/misc/com.google.code.gson.gson-$GsonVersion.ane?raw=true
 Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.tuarua.frekotlin-$FreKotlinVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/kotlin/com.tuarua.frekotlin-$FreKotlinVersion.ane?raw=true
+Invoke-WebRequest -OutFile "$currentDir\android_dependencies\android.arch.lifecycle.runtime-$LifeCycleVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/play-services/android.arch.lifecycle.runtime-$LifeCycleVersion.ane?raw=true
+Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.android.support.support-v4-$SupportV4Version.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/support/com.android.support.support-v4-$SupportV4Version.ane?raw=true
+Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.android.support.appcompat-v7-$SupportV4Version.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/support/com.android.support.appcompat-v7-$SupportV4Version.ane?raw=true
+Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.android.support.cardview-v7-$SupportV4Version.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/support/com.android.support.cardview-v7-$SupportV4Version.ane?raw=true
+Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.android.support.customtabs-$SupportV4Version.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/support/com.android.support.customtabs-$SupportV4Version.ane?raw=true
+Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.parse.bolts.bolts-android-$BoltsVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/facebook/com.parse.bolts.bolts-android-$BoltsVersion.ane?raw=true
+Invoke-WebRequest -OutFile "$currentDir\android_dependencies\com.facebook.android.facebook-android-sdk-$FacebookVersion.ane" -Uri https://github.com/tuarua/Android-ANE-Dependencies/blob/master/anes/facebook/com.facebook.android.facebook-android-sdk-$FacebookVersion.ane?raw=true
