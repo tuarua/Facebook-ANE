0b040e07c467d4c00e6287820d6f7ab5550e0fba
Eoin Landy <info@tuarua.co.uk>
2020-08-22 23:30:24 +0100

    1.4.0

--- example/get_android_dependencies.ps1
+++ example/get_android_dependencies.ps1
@@ -1,5 +1,5 @@
-$AneVersion = "1.3.0"
-$FreKotlinVersion = "1.10.0"
+$AneVersion = "1.4.0"
+$FreKotlinVersion = "1.40.0"
 $GsonVersion = "2.8.6"
 $SupportV4Version = "1.0.0"
 $BoltsVersion = "1.4.0"
